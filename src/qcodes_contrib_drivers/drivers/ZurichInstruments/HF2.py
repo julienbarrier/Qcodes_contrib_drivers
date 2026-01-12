c47381a08e85ac360e7a522e85dc5e99a301a246
Julien Barrier <julien.barrier@icfo.eu>
2026-01-12 16:35:38 +0100

    debug

--- src/qcodes_contrib_drivers/drivers/ZurichInstruments/HF2.py
+++ src/qcodes_contrib_drivers/drivers/ZurichInstruments/HF2.py
@@ -1,15 +1,12 @@
-from qcodes.validators import ComplexNumbers
-from qcodes.parameters import ParamRawDataType, Parameter
+from qcodes.utils.validators import ComplexNumbers
+from qcodes.instrument.parameter import ParamRawDataType
 from typing import Any, Optional
-from zhinst.qcodes import MFLI as mfli
-
+from qcodes import Parameter
+from zhinst.qcodes import HF2 as hf2li
 
 class ComplexSampleParameter(Parameter):
     def __init__(
-        self,
-        *args: Any,
-        dict_parameter: Optional[Parameter] = None,
-        **kwargs: Any
+        self, *args: Any, dict_parameter: Optional[Parameter] = None, **kwargs: Any
     ):
         super().__init__(*args, **kwargs)
         if dict_parameter is None:
@@ -19,13 +16,12 @@ class ComplexSampleParameter(Parameter):
     def get_raw(self) -> ParamRawDataType:
         values_dict = self._dict_parameter.get()
         return complex(values_dict["x"][0], values_dict["y"][0])
-
-
-class MFLI(mfli):
+        
+class HF2(hf2li):
     """
-    This wrapper adds back a "complex sample" parameter to the demodulators
-    such that we can use them in the way that it was done with "sample"
-    parameter in version 0.2 of ZHINST-qcodes
+    This wrapper adds back a "complex sample" parameter to the demodulators such that
+    we can use them in the way that we have done with "sample" parameter
+    in version 0.2 of ZHINST-qcodes
     written by jenshnielsen: https://github.com/zhinst/zhinst-qcodes/issues/41
     """
 
@@ -37,6 +33,7 @@ class MFLI(mfli):
             demod.add_parameter(
                 "complex_sample",
                 label="Vrms",
+                unit='V',
                 vals=ComplexNumbers(),
                 parameter_class=ComplexSampleParameter,
                 dict_parameter=demod.sample,
